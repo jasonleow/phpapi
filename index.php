96b5b72f3f5500a480f66738266c34bff13909ed
Jason Leow <35625566+jasonleow@users.noreply.github.com>
2024-06-21 18:57:50 +0800

    Change sql query to a prepared statement with a parameterized query

--- index.php
+++ index.php
@@ -4,11 +4,14 @@ header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Origin: *");
 
 $handle = new SQLite3("data.db");
-$db = "student";
-$res = $handle->query("select * from " . $db);
+
+// Use a prepared statement with a parameterized query
+$stmt = $handle->prepare("SELECT * FROM :db");
+$stmt->bindValue(":db", "student", SQLITE3_TEXT);
+$result = $stmt->execute();
 
 $data = [];
-while ($row = $res->fetchArray(SQLITE3_ASSOC)) {
+while ($row = $result->fetchArray(SQLITE3_ASSOC)) {
     array_push($data, $row);
 }
 
